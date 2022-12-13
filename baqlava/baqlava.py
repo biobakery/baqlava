232fd3b466f3c40fa6df3d78dacc5158f202f7f3
sagun <sagun_90@hotmail.com>
2022-12-13 13:13:54 -0500

    updated setup.py

--- baqlava/baqlava.py
+++ baqlava/baqlava.py
@@ -22,6 +22,35 @@ from glob import glob
 from anadama2 import Workflow
 from anadama2.tracked import TrackedExecutable
 
+
+# Config Parsers 
+# try to import the python2 ConfigParser
+# if unable to import, then try to import the python3 configparser
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
+config = configparser.ConfigParser()
+config.read('configs/baqlava.cfg')
+
+WORKFLOW_FOLDER = "."
+WORKFLOW_EXTENSION = ".py"
+
+
+def find_workflows():
+	""" Search for installed workflows """
+
+	workflow_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), WORKFLOW_FOLDER)
+	workflows = {}
+	for file in os.listdir(workflow_folder):
+		# look for files with the expected extension
+		if file.endswith(WORKFLOW_EXTENSION):
+			# do not need to add full path as these are also installed as executable scripts
+			workflows[file.replace(WORKFLOW_EXTENSION, "")] = file
+
+	return workflows
+
 # Setting the version of the workflow and short description
 workflow = Workflow(
     version="0.0.1",                    #Update the version as needed
@@ -33,37 +62,37 @@ workflow = Workflow(
 ###############
 # custom args #
 ###############
-
 workflow.add_argument(
     name = "nucdb",
     desc = "nucleotide database folder to use",
-    default = config.nucleotide_db)
+    default = config.get('database','nucleotide_db'))
 
 workflow.add_argument(
     name = "nucindex",
     desc = "nucleotide annotation index to use",
-    default = config.idmap)
+    default = config.get('utility','idmap'))
 
 workflow.add_argument(
     name = "threads",
     desc="number of threads for knead_data to use",
-    default=config.threads)
+    default=config.get('computation','threads'))
 
 workflow.add_argument(
     name = "protdb",
     desc = "protein database folder to use",
-    default = config.proteindb)
+    default = config.get('database','uniref_db'))
 
 args = workflow.parse_args()
 
-############################
-# function to run workflow #
-############################
 
-file_base = args.input.split("/")[-1].split(".")[0]
-output_dir = args.output + "/" + file_base + "/"
+def main():
+    ############################
+    # function to run workflow #
+    ############################
+    file_base = args.input.split("/")[-1].split(".")[0]
+    output_dir = args.output + "/" + file_base + "/"
 
-workflow.add_task(
+    workflow.add_task(
     "humann --input [depends[0]] --output [output_folder[0]] --bypass-nucleotide-index --nucleotide-database [n_db] --id-mapping [idx] --protein-database [p_db] --threads [threads]",
     depends = [args.input],
     output_folder = [output_dir],
@@ -73,11 +102,15 @@ workflow.add_task(
     p_db = args.protdb,
     threads = args.threads)
 
-workflow.add_task(
-    "python reconcile_mapped_reads.py [depends[0]]" ,
-    depends = [output_dir + file_base + "_genefamilies.tsv"],
-    targets = [output_dir + file_base + "_baqlava_genefamilies.tsv"],
-    output_folder = output_dir,
-    script = args.reconcile)
+    workflow.add_task(
+        "python reconcile_mapped_reads.py [depends[0]]" ,
+        depends = [output_dir + file_base + "_genefamilies.tsv"],
+        targets = [output_dir + file_base + "_baqlava_genefamilies.tsv"],
+        output_folder = output_dir,
+        script = args.reconcile)
 
-workflow.go()
+    workflow.go()
+
+
+if __name__ == "__main__":
+	main()
