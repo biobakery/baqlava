6924507cf35ecb078586ad8e61e3f7435caf00c9
jjensen44 <64109894+jjensen44@users.noreply.github.com>
2024-05-31 15:44:11 -0400

    Update join_tables.py

--- baqlava/utility_scripts/join_tables.py
+++ baqlava/utility_scripts/join_tables.py
@@ -3,13 +3,15 @@ import os
 import sys
 
 def main():
-    retdf = pd.DataFrame({'Species':[]})
+    retdf = pd.DataFrame({'BAQLaVa VGB':[]})
     for i in os.listdir(sys.argv[1]):
-        if "_BAQLaVa_profile.tsv" in i:
+        if "_BAQLaVa_profile.txt" in i:
             name = i.split("_BAQLaVa_")[0] + "_Abundance-RPKs"
-            colname = i.split("_BAQLaVa_")[0] + "_bowtie2_unaligned_lengthremoved_Abundance-RPKs"
-            tempdf = pd.read_csv(sys.argv[1] + i, sep="\t").query("Database=='TOTAL'").drop(columns=['Database']).rename(columns={colname:name})
-            retdf = pd.merge(retdf, tempdf, on='Species', how='outer')
+            tempdf = pd.read_csv(sys.argv[1] + i, sep="\t")
+            tempdf = tempdf[~tempdf['BAQLaVa VGB'].str.contains("\|")]
+            colname = tempdf.columns[1]
+            tempdf = tempdf[['BAQLaVa VGB',colname]].rename(columns={colname:name})
+            retdf = pd.merge(retdf, tempdf, on='BAQLaVa VGB', how='outer')
     a = retdf.fillna(0)
     a.to_csv(sys.argv[2] + "BAQLaVa_VGB_table.tsv", sep="\t", index=False)
     return None
